eed5204db1205d3d1a0ec16a54a110b607de4df0
vishnurobosoft <vishnumoorthi@Vishnumoorthi-MBP.local>
2015-07-30 12:01:49 +0530

    Added a dummy project.

---
+++ Swift-Demo/Swift-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-Demo
+//
+//  Created by Vishnumoorthi Bhat G on 30/07/15.
+//  Copyright (c) 2015 Robosoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
